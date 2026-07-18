ed2f2d5d6fbea662649a6f00eca8baa72e2ffc87
Kovács <kmarton2000@gmail.com>
2026-07-18 14:39:11 +0200

    Add execute perminnion to sh scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,6 @@ microk8s helm3 upgrade --install argocd . \
 
 echo
 echo "Initial password:"
-microk8s kubectl -n argocd get secret argocd-initial-admin-secret \
-    -o jsonpath="{.data.password}" | base64 -d
+#microk8s kubectl -n argocd get secret argocd-initial-admin-secret \
+#    -o jsonpath="{.data.password}" | base64 -d
 echo
